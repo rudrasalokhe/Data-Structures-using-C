e2a60ef273b6bb1c20891d8b021268df860ac852
Rudra Salokhe <133947299+rudrasalokhe@users.noreply.github.com>
2023-11-20 20:15:17 +0530

    Update insertdeletequeue.c

--- insertdeletequeue.c
+++ insertdeletequeue.c
@@ -1,56 +1,56 @@
-// wap to insert and delete in queue
 #include<stdio.h>
 #include<conio.h>
 #define MAX 5
 int queue[MAX];
-int front = -1 , rear = -1;
+int front = - 1, rear = -1 ;
 void insert();
-void detete();
+void delete();
 void display();
-void main(){
+int  main(){
   int choice;
-  while(1){
-    printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit");
-    scanf("%d", &choice);
-    switch(choice){
-      case 1:
-      insert();
-      break;
-      case 2:
-      delete();
-      break;
-      case 3:
-      display();
-      break;
-      case 4:
-      exit(0);
-    }
+  printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit");
+  scanf("%d", &choice);
+  switch(choice){
+    case 1:
+    insert();
+    break;
+    case 2:
+    delete();
+    break;
+    case 3: 
+    display();
+    break;
+    case 4:
+    exit(0);
+
+
   }
 }
 void insert(){
   int item;
-  if (rear == MAX-1)
-  printf("Queue is full");
-  else {
-  if(rear == -1){
-    front =0;
+  if(rear = MAX-1){
+    printf("Queue is Full");
   }
-  printf("Element to be inserted :");
+  else{
+  if(rear == - 1 ){
+    front  = 0;
+  }
+  printf("Enter the item you want to insert:\n");
   scanf("%d", &item);
-  rear=rear+1;
+  rear = rear + 1 ;
   queue[rear]=item;
-  
 }
 }
-oid delete () {
-  if (front == -1 || front > rear)
-    printf("\nQueue is empty");
-  else {
-    printf("\nDeleted element is %d", queue[front]);
-    front = front + 1;
+void display(){
+  if( front == -1 || front > rear ){
+    printf("Queue is empty");
   }
-}
+  else{
+  printf("%d is is deleted from ", queue[front]);
+  front = front + 1;
 
+}
+}
 void display() {
   int i;
   if (front == -1)
